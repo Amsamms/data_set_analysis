8b548dd44d7fc6623b2b07879dcd3214b0192d26
John Doeahmed <ahmedsabri85@gmail.com>
2023-07-07 14:42:45 +0300

    add some title and how to use

--- data_set_analysis.py
+++ data_set_analysis.py
@@ -3,6 +3,20 @@ import pandas as pd
 import numpy as np
 from ydata_profiling import ProfileReport
 
+# Add a title
+st.title("Data Analysis Simplified")
+
+# Add a catchy subtitle
+st.markdown("## Unleash the power of data with a single click!")
+
+# Add a section on how to use the app
+st.markdown("""
+### How to use this app:
+1. Upload your dataset (Make sure the first row contains column names).
+2. Click on 'Generate report' and wait for a while.
+3. Click 'Download' to download the generated report.
+""")
+
 data= st.file_uploader("Choose excel or csv file to upload",type=['csv','xls','xlsx'],key='1')
 if data is not None:
     try:
